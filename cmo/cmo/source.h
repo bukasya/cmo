da3a14b3516759c486188b255e5212a211716c52
bukasya <bukasya@live.ru>
2017-12-11 05:04:31 +0300

    fixed many issues with placing, deleting and updating requests in system; updated statistics

--- cmo/cmo/source.h
+++ cmo/cmo/source.h
@@ -8,8 +8,13 @@ public:
 	~Source();
 	Request generate_request();
 	int get_number();
-
+	int get_generated_reqs();
+	void increment_generated_reqs();
+	int get_declined_reqs();
+	void increment_declined_reqs();
 private:
 	int number; //sets on creation
+	int generated_reqs;
+	int declined_reqs;
 };
 
