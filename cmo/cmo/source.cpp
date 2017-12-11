fc0fbe0f61d619b30e4973071b1645727c792b7c
bukasya <bukasya@live.ru>
2017-12-11 05:04:31 +0300

    fixed many issues with placing, deleting and updating requests in system; updated statistics

--- cmo/cmo/source.cpp
+++ cmo/cmo/source.cpp
@@ -5,6 +5,8 @@
 Source::Source(int number)
 {
 	Source::number = number;
+	Source::generated_reqs = 0;
+	Source::declined_reqs = 0;
 }
 
 Source::~Source()
@@ -19,4 +21,24 @@ Request Source::generate_request()
 int Source::get_number()
 {
 	return number;
+}
+
+int Source::get_generated_reqs()
+{
+	return this->generated_reqs;
+}
+
+void Source::increment_generated_reqs()
+{
+	this->generated_reqs += 1;
+}
+
+int Source::get_declined_reqs()
+{
+	return this->declined_reqs;
+}
+
+void Source::increment_declined_reqs()
+{
+	this->declined_reqs += 1;
 }
